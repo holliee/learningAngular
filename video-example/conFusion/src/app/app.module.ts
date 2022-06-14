1969e600dd3be5fa728644564b219cf7478a3110
Hollie O'Brien <hobrien@seas.upenn.edu>
2022-06-13 19:05:10 -0700

    mat button and card modules

--- video-example/conFusion/src/app/app.module.ts
+++ video-example/conFusion/src/app/app.module.ts
@@ -12,6 +12,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import{ MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +30,10 @@ import {MatListModule} from '@angular/material/list';
     MatSliderModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
